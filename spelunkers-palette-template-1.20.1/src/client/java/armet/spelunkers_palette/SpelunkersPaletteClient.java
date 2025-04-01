b7c58796aecf6fdfb0a1521420a3e3c0cc09057d
armet0451 <scourgebeast97@gmail.com>
2025-04-01 12:01:12 -0400

    Add files via upload
    initial commit

---
+++ spelunkers-palette-template-1.20.1/src/client/java/armet/spelunkers_palette/SpelunkersPaletteClient.java
@@ -0,0 +1,10 @@
+package armet.spelunkers_palette;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpelunkersPaletteClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ spelunkers-palette-template-1.20.1/src/client/java/armet/spelunkers_palette/SpelunkersPaletteDataGenerator.java
@@ -0,0 +1,11 @@
+package armet.spelunkers_palette;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpelunkersPaletteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
